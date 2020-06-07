a6ca151f2acaa2a3f3e19252b64e1af4798b8c8c
c5apple <c5apple_party@yahoo.co.jp>
2020-06-08 00:20:38 +0900

    ミノ画像

--- src/app/shared/service/mino.ts
+++ src/app/shared/service/mino.ts
@@ -18,6 +18,12 @@ export class Mino {
   /** 形 */
   shape: string;
 
+  /** 画像URL */
+  imgUrl: string;
+
+  /** 一番目の画像URL */
+  imgUrlNext: string;
+
   /** すべてのNoを取得する */
   public static getNoList(): number[] {
     return [1, 2, 3, 4, 5, 6, 7];
@@ -33,24 +39,38 @@ export class Mino {
     switch (no) {
       case 1:
         this.shape = 'I';
+        this.imgUrl = '/assets/img/i.png';
+        this.imgUrlNext = '/assets/img/i-next.png';
         break;
       case 2:
         this.shape = 'J';
+        this.imgUrl = '/assets/img/j.png';
+        this.imgUrlNext = '/assets/img/j-next.png';
         break;
       case 3:
         this.shape = 'L';
+        this.imgUrl = '/assets/img/l.png';
+        this.imgUrlNext = '/assets/img/l-next.png';
         break;
       case 4:
         this.shape = 'O';
+        this.imgUrl = '/assets/img/o.png';
+        this.imgUrlNext = '/assets/img/o-next.png';
         break;
       case 5:
         this.shape = 'S';
+        this.imgUrl = '/assets/img/s.png';
+        this.imgUrlNext = '/assets/img/s-next.png';
         break;
       case 6:
         this.shape = 'T';
+        this.imgUrl = '/assets/img/t.png';
+        this.imgUrlNext = '/assets/img/t-next.png';
         break;
       case 7:
         this.shape = 'Z';
+        this.imgUrl = '/assets/img/z.png';
+        this.imgUrlNext = '/assets/img/z-next.png';
         break;
     }
   }
