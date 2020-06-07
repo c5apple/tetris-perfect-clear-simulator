5867ad2d15ee471e69688d7e39398bb0cc91da85
c5apple <c5apple_party@yahoo.co.jp>
2020-06-07 22:38:52 +0900

    Nextミノとボタンのモック

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -10,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayComponent implements OnInit {
 
+  /** 答えたか */
+  answerShowed = false;
+
+  /** 正誤判定1 */
+  anserMark1: number;
+  /** 正誤判定2 */
+  anserMark2: number;
+  /** 正誤判定3 */
+  anserMark3: number;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * 答えるボタン
+   * @param buttonId 1:ある, 2:ある(ホールドなし), 3:ない
+   */
+  answer(buttonId: number): void {
+    if (this.answerShowed) {
+      return;
+    }
+    this.answerShowed = true;
+
+    // 正誤判定
+    // this.anserMark1 = 1;
+    this.anserMark2 = 1;
+    this.anserMark3 = 2;
+  }
 }
