53ade3fc5eadb2887b3a103ab58ea411f7afd2a5
c5apple <c5apple_party@yahoo.co.jp>
2020-06-08 17:51:51 +0900

    no follow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 import { SwUpdate } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor(
+    private router: Router,
+    private meta: Meta,
     private swUpdate: SwUpdate
   ) {
     if (this.swUpdate.isEnabled) {
@@ -19,5 +24,17 @@ export class AppComponent {
       // Check for new version
       this.swUpdate.checkForUpdate();
     }
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((params: any) => {
+      window.scrollTo(0, 0);
+
+      if (params.url !== '/') {
+        this.meta.addTag({ name: 'robots', content: 'noindex' });
+      } else {
+        this.meta.removeTag('name=robots');
+      }
+    });
   }
 }
