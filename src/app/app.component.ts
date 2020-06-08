39dba6293efd873bed5c4c66193bdf88530fd6db
c5apple <c5apple_party@yahoo.co.jp>
2020-06-08 16:32:12 +0900

    service-worker 更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private swUpdate: SwUpdate
+  ) {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        // 強制更新
+        window.location.reload();
+      });
+      // Check for new version
+      this.swUpdate.checkForUpdate();
+    }
+  }
 }
