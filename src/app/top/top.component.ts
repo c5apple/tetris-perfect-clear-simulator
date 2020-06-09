15630a6dd539c41ac6017b51ab305d3b2a4f09fe
c5apple <c5apple_party@yahoo.co.jp>
2020-06-10 00:21:59 +0900

    リダイレクト時に一瞬文字が見えるバグ

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TranslateService, DefaultLangChangeEvent } from '@ngx-translate/core';
 
 /**
  * トップ画面
@@ -14,13 +15,29 @@ export class TopComponent implements OnInit {
   /** 言語 */
   lang = '';
 
+  /** 説明文 */
+  description = '';
+
+  /** スタートボタン */
+  start = '';
+
   constructor(
     private route: ActivatedRoute,
+    private translate: TranslateService
   ) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: { lang: string }) => {
       this.lang = params.lang;
+
+      if (this.translate.getDefaultLang()) {
+        this.translate.get('説明文').subscribe(description => {
+          this.description = description;
+        });
+        this.translate.get('スタート').subscribe(start => {
+          this.start = start;
+        });
+      }
     });
   }
 
