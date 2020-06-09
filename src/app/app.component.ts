61b4a81e5e12d2f7abd6dadbe2ad25cb5710bb62
c5apple <c5apple_party@yahoo.co.jp>
2020-06-09 18:05:25 +0900

    URLから言語切り替え

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Meta } from '@angular/platform-browser';
 import { SwUpdate } from '@angular/service-worker';
-import { filter } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,11 @@ import { filter } from 'rxjs/operators';
 export class AppComponent implements OnInit {
 
   constructor(
+    private route: ActivatedRoute,
     private router: Router,
     private meta: Meta,
-    private swUpdate: SwUpdate
+    private swUpdate: SwUpdate,
+    private translate: TranslateService
   ) {
     if (this.swUpdate.isEnabled) {
       this.swUpdate.available.subscribe(() => {
@@ -27,14 +30,44 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((params: any) => {
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.route),
+      map(route => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter(route => route.outlet === 'primary'),
+      mergeMap(route => route.params)
+    ).subscribe((params: { lang: string }) => {
       window.scrollTo(0, 0);
 
+      // URLの言語チェック
+      this.checkLang(params.lang);
+    });
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((params: any) => {
       if (params.url !== '/') {
         this.meta.addTag({ name: 'robots', content: 'noindex' });
       } else {
         this.meta.removeTag('name=robots');
       }
     });
+  }
+
+  /** サポート言語 */
+  private availableLangList = ['en', 'ja'];
+
+  /**
+   * 言語チェック
+   */
+  private checkLang(lang: string) {
+    if (this.availableLangList.indexOf(lang) === -1) {
+      // デフォルト英語
+      return window.location.href = './en/';
+    }
+    // 言語設定
+    this.translate.setDefaultLang(lang);
   }
 }
